e6a6cdc28d26af92bf2b1d3603c554adcab7c989
Martin Alker <alker@punkt.de>
2020-10-16 19:12:05 +0200

    Change Tailwind CSS integration

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -1,11 +1,11 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Cairo', ...defaultTheme.fontFamily.sans],
-            },
-        },
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Cairo', ...defaultTheme.fontFamily.sans],
+      },
     },
+  },
 };
